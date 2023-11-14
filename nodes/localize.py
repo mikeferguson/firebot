2e089a7cccd72d245cd44bd3a7e17077f5b16d29
Michael Ferguson <mfergs7@gmail.com>
2023-11-14 14:34:25 -0500

    refactor localization

--- nodes/localize.py
+++ nodes/localize.py
@@ -6,18 +6,29 @@ from geometry_msgs.msg import PoseWithCovarianceStamped
 
 from poses import HOME
 
+
+class Localization:
+
+    def __init__(self, node):
+        self.pub = node.create_publisher(PoseWithCovarianceStamped, "initialpose", 1)
+        self.node = node
+
+    def localize_at_home(self):
+        msg = PoseWithCovarianceStamped()
+        msg.header.frame_id = "map"
+        msg.header.stamp = self.node.get_clock().now().to_msg()
+        msg.pose.pose = HOME
+        msg.pose.covariance[0] = 0.0125
+        msg.pose.covariance[7] = 0.0125
+        msg.pose.covariance[35] = 0.06853891909122467
+        self.pub.publish(msg)
+
+
 if __name__=="__main__":
     rclpy.init()
     node = Node("localize")
-    pub = node.create_publisher(PoseWithCovarianceStamped, "initialpose", 1)
 
-    msg = PoseWithCovarianceStamped()
-    msg.header.frame_id = "map"
-    msg.header.stamp = node.get_clock().now().to_msg()
-    msg.pose.pose = HOME
-    msg.pose.covariance[0] = 0.0125
-    msg.pose.covariance[7] = 0.0125
-    msg.pose.covariance[35] = 0.06853891909122467
+    localize = Localization(node)
+    localize.localize_at_home()
 
-    pub.publish(msg)
     rclpy.spin(node)
