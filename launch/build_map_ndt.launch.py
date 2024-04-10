1ffa7010834b36eb09414536ef2521340df2d55d
Michael Ferguson <mfergs7@gmail.com>
2024-04-10 13:07:29 -0400

    commit ndt mapping launch

---
+++ launch/build_map_ndt.launch.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # Get the launch directory
+    bringup_dir = get_package_share_directory('firebot')
+    lifecycle_nodes = ['map_server', 'amcl']
+
+    return LaunchDescription([
+
+        Node(
+            package='nav2_map_server',
+            executable='map_server',
+            name='map_server',
+            output='screen',
+            parameters=[{'yaml_filename':
+                         os.path.join(bringup_dir, 'config', 'map_shrunk_arena.yaml')}]),
+
+        Node(
+            package='nav2_amcl',
+            executable='amcl',
+            name='amcl',
+            output='screen',
+            parameters=[os.path.join(bringup_dir, 'config', 'nav2_params.yaml')]),
+
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_localization',
+            output='screen',
+            parameters=[{'autostart': True},
+                        {'node_names': lifecycle_nodes}])
+    ])
